827da756135ad5072eb32683dc0588da1c770959
David Lai <davidlai@universal.com.tw>
2020-03-25 16:36:35 +0800

    add users model and migrations

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -16,5 +16,15 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
+    /* 
+      * Migrations
+        ! need to install dotnet-ef tool and package: Microsoft.EntityFrameworkCore.Design
+        ::`dotnet ef migrations add InitialCreate`
+        :: to apply migrations, run `dotnet ef database update`
+
+        ? When should you run migrations and update db after initial create?
+          * whenever you make changes to your models (create a new class / change existing class properties).
+    */
 }
