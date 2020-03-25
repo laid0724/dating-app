3a232ad58875a6670f88a9447f629cd39b302eae
David Lai <davidlai@universal.com.tw>
2020-03-25 12:38:11 +0800

    setup DataContext and install entity framework with sqlite

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+// ? this is for DB connections. 
+// ? You need to install and import entity framework core package for this to work. 
+
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    /* 
+      this is the required syntax for adding a DbContext class.
+      then, you need to inject this into ConfigureServices in Startup.cs
+        services.AddDbContext<DataContext>();
+    */
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +31,12 @@ namespace DatingApp.API
         // ? this is for injecting services
         public void ConfigureServices(IServiceCollection services)
         {
+            // ? Injects DataContext to connect to a db
+            services.AddDbContext<DataContext>(
+              // need to install sqlite entity package for this
+              data => data.UseSqlite("ConnectionString")
+            );
+
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
