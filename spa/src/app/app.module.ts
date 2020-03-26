fe8e97dd9c1112535930c640f5e54d58ae8bea0c
David Lai <davidlai@universal.com.tw>
2020-03-26 16:17:20 +0800

    UPDT .gitignore

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
+  declarations: [AppComponent, ValueComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
