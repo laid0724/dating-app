80bc2761d2b079276503ca35beae09e047d42692
David Lai <laid0724@gmail.com>
2021-06-03 19:44:01 +0800

    UPDT downgrade to angular 10

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('/api/users').subscribe((users) => {
+      this.users = users;
+    });
+  }
+}
