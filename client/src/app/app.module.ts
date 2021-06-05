261d7076c2508839ce7dded9184c49cf55cbcba2
David Lai <laid0724@gmail.com>
2021-06-05 23:25:24 +0800

    IMPL angular: http errors interceptor to handle http errors globally

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './modules/shared.module';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,13 @@ import { SharedModule } from './modules/shared.module';
     ReactiveFormsModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ client/src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
+
+/*
+  Provide this in your app module:
+
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
+*/
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error) {
+          switch (error.status) {
+            case 400: // bad requests
+              const errors = error.error.errors; // for validation errors, which is stored as array with several dimensions
+              if (errors) {
+                const modalStateErrors = [];
+                for (const key in errors) {
+                  if (errors[key]) {
+                    modalStateErrors.push(errors[key]);
+                  }
+                }
+                throw modalStateErrors.flat(Infinity);
+              } else {
+                this.toastr.error(error.statusText, error.status.toString());
+              }
+              break;
+            case 401: // Unauthorized
+              this.toastr.error(error.statusText, error.status.toString());
+              break;
+            case 404: // Not Found
+              // NOTE: be careful with this - your APIs cannot throw 404 unless it really is a case of 404.
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500: // Internal Server Error
+              // here we redirect as well but we add on NavigationExtras as a message to be passed to the page
+              const navigationExtras: NavigationExtras = {
+                state: {
+                  error: error.error,
+                },
+              };
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+            default:
+              this.toastr.error('Something unexpected went wrong.');
+              console.log('Error Caught::', error);
+              break;
+          }
+          return throwError(error);
+        }
+      })
+    );
+  }
+}
