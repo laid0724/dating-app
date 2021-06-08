453b4b40b700fda6870a15ede5da610e162a45c8
David Lai <laid0724@gmail.com>
2021-06-08 22:15:51 +0800

    UPDT angular: install angular cdk and ngx-spinner

--- client/src/app/modules/shared.module.ts
+++ client/src/app/modules/shared.module.ts
@@ -4,6 +4,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 const ngxBootstrapModules = [BsDropdownModule.forRoot(), TabsModule.forRoot()];
 
@@ -16,7 +17,14 @@ const ngxBootstrapModules = [BsDropdownModule.forRoot(), TabsModule.forRoot()];
       positionClass: 'toast-bottom-right',
     }),
     NgxGalleryModule, // see: https://github.com/kolkov/ngx-gallery
+    NgxSpinnerModule, // see: https://github.com/Napster2210/ngx-spinner
   ],
-  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule,
+    NgxSpinnerModule,
+  ],
 })
 export class SharedModule {}
