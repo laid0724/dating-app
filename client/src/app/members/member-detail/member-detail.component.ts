208d310b753d068420025784c557093fc3c4a46e
David Lai <laid0724@gmail.com>
2021-06-08 20:31:32 +0800

    IMPL angular: install ngx-gallery and impl it to member-detail

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  NgxGalleryAnimation,
+  NgxGalleryImage,
+  NgxGalleryOptions,
+} from '@kolkov/ngx-gallery';
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/services/members.service';
 
@@ -11,6 +16,9 @@ import { MembersService } from 'src/app/services/members.service';
 export class MemberDetailComponent implements OnInit {
   member: Member;
 
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   constructor(
     private membersService: MembersService,
     private route: ActivatedRoute
@@ -18,11 +26,37 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+  }
+
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url,
+      });
+    }
+    return imageUrls;
   }
 
   loadMember(): void {
     this.membersService
       .getMember(this.route.snapshot.paramMap.get('username'))
-      .subscribe((member) => (this.member = member));
+      .subscribe((member) => {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      });
   }
 }
--- client/src/app/modules/shared.module.ts
+++ client/src/app/modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 const ngxBootstrapModules = [BsDropdownModule.forRoot(), TabsModule.forRoot()];
 
@@ -14,7 +15,8 @@ const ngxBootstrapModules = [BsDropdownModule.forRoot(), TabsModule.forRoot()];
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
+    NgxGalleryModule, // see: https://github.com/kolkov/ngx-gallery
   ],
-  exports: [BsDropdownModule, ToastrModule, TabsModule],
+  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
 })
 export class SharedModule {}
