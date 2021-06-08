1ebc9571807a24506ec4a12c2cde539a049a94d7
David Lai <laid0724@gmail.com>
2021-06-08 15:33:54 +0800

    UPDT .net: Update Program.cs Main method to automatically apply migrations and seed data on launch

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +14,38 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            /*
+                here, we are reformating this so that we can get access to the database
+                and inject it into seeders before the rest of the program is spun up.
+
+                also set the program to automatically create db / apply migrations
+                
+            */
+            var host = CreateHostBuilder(args).Build();
+            
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+
+                // this will automatically run dotnet ef database update
+                await context.Database.MigrateAsync();
+
+                // run all seeders here:
+                await Seed.SeedUsers(context);
+            }
+            catch(Exception ex)
+            {
+                // this is to catch all error related to migrations/seeding
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
