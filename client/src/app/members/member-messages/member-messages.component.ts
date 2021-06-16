f2955d30fa974cd633aa928f108542f7dd507ff0
David Lai <laid0724@gmail.com>
2021-06-16 15:47:32 +0800

    UPDT angular: limit height of member-messages component and scroll to bottom by default

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/models/message';
 import { MessageService } from 'src/app/services/message.service';
@@ -11,11 +17,35 @@ import { MessageService } from 'src/app/services/message.service';
 export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm: NgForm;
 
+  // see: https://stackoverflow.com/questions/39366981/viewchild-in-ngif
+  private chatMessagesContainer: ElementRef<HTMLDivElement>;
+  @ViewChild('chatMessagesContainer', { static: false }) set content(
+    content: ElementRef<HTMLDivElement>
+  ) {
+    if (content) {
+      // initially setter gets called with undefined
+      this.chatMessagesContainer = content;
+
+      const chatMessagesContainerEl: HTMLDivElement =
+        this.chatMessagesContainer.nativeElement;
+
+      // scroll to bottom of the chat window
+      this.renderer.setProperty(
+        chatMessagesContainerEl,
+        'scrollTop',
+        chatMessagesContainerEl.scrollHeight
+      );
+    }
+  }
+
   @Input() username: string;
   // @Input() messages: Message[];
   messageContent: string;
 
-  constructor(public messageService: MessageService) {}
+  constructor(
+    public messageService: MessageService,
+    private renderer: Renderer2
+  ) {}
 
   sendMessage(): void {
     // this.messageService
